4dca4534fa8d898510e70f90df7d34100b29fae1
Hassan Kashif <hassank1751@gmail.com>
2025-01-13 23:47:19 -0800

    Refactor CompanyHistory component for improved text clarity and styling

--- src/app/about/_components/CompanyHistory.tsx
+++ src/app/about/_components/CompanyHistory.tsx
@@ -29,16 +29,16 @@ const CompanyHistory = () => {
             height={80}
             alt="About-Zyck"
           />
-          <p className="title-font text-lg sm:text-xl md:text-2xl mb-5 font-semibold text-white">
+          <p className="title-font text-lg sm:text-xl md:text-xl mb-5 font-semibold text-white">
             At ZYCK Property, we believe finding the right property is not just about a house but rather about finding a space that matches your lifestyle, values, and dreams, where community, convenience, and quality come together to create a true sense of home.
           </p>
-          <p className="mb-8 leading-relaxed text-sm sm:text-base md:text-lg text-gray-100">
+          <p className="mb-8 text-gray-100 leading-relaxed">
             That is why we go beyond the typical listings, by sourcing insights
             straight from locals and offering over 34 neighborhood map overlays, to
             give people a deeper understanding of what living in a home and
             neighborhood is really like.
           </p>
-          <p className="mb-8 leading-relaxed text-sm sm:text-base md:text-lg text-gray-100">
+          <p className="mb-8 text-gray-100 leading-relaxed">
             Our mission is to help people find a place where they can build meaningful connections and feel truly at home, fostering a stronger sense of community and belonging. Every day, we work to create a more neighborly and connected world.
           </p>
           <div className="flex justify-center">
