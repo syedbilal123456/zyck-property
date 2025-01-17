d798cd39d70f8056fb970a6a37d084072d3e02e4
Abdul-Majeed-Ansari <abdulmajeed31136@gmail.com>
2025-01-17 03:07:46 -0800

    content added

--- src/app/privacy-policy/_components/ContentCard.tsx
+++ src/app/privacy-policy/_components/ContentCard.tsx
@@ -3,43 +3,28 @@ const ContentCard = () => {
       <div className="max-w-screen-lg mx-auto bg-card border border-gray-300 rounded-lg p-8 shadow-lg my-14">
         <h2 className="text-xl font-bold text-gray-800 mb-4">Privacy Policy</h2>
         <p className="mb-4 leading-relaxed text-gray-700">
-          We take your privacy very seriously and are committed to protecting the
-          privacy of all visitors and subscribers to our website{" "}
+        We fee your privacy and are devoted to safeguarding the personal records of all traffic and users of our internet site, {" "}
           <a
-            href="https://www.zameen.com"
+            href="https://www.Zyckproperty.Com"
             className="text-blue-600 font-semibold hover:underline"
           >
             www.zyckproperty.com
           </a>{" "}
-          (the <span className="font-semibold">"Website"</span>) or any mobile
-          application we make available via an app store (the{" "}
-          <span className="font-semibold">"Application"</span>, together with the
-          Website, the <span className="font-semibold">"Platform"</span>), and the
-          corresponding services available through the Platform (the{" "}
-          <span className="font-semibold">"Services"</span>).
+          (the <span className="font-semibold">"Website",</span>) and any cell utility we provide through app shops (the "Application" together with the Website, the "Platform"). This privacy policy outlines how we protect and shield your records while you get the right of entry to the Platform or use its related offerings (the "Services").
         </p>
         <p className="mb-4 leading-relaxed text-gray-700">
-          Below we set out our privacy policy, which will govern the way in which
-          we process any personal information that you provide to us.
+        Please take some time to study this privacy policy carefully as it presents vital facts about who we are and how we collect, save, use, and percentage your statistics. By having access to the Platform, the use of the Services, or in any other case, providing your consent, you agree to and, where applicable, offer consent for collecting, utilizing, and sharing your information as described in this policy. If you no longer consider the phrases of this privacy policy, you must refrain from the use of the Platform and/or the Services. This privacy policy enhances other notices or privacy rules we may problem and isn't always supposed to override them.
         </p>
         <p className="mb-4 leading-relaxed text-gray-700">
-          Please read this privacy policy carefully as it contains important
-          information on who we are and how we collect, store, use, and share your
-          information. By accessing the Platform or using our Services or
-          otherwise indicating your consent, you agree to, and where required,
-          consent to the collection, use, and transfer of your information as set
-          out in this policy. If you do not accept the terms of this policy, you
-          must not use the Platform and/or the Services. This privacy policy
-          supplements other notices and privacy policies and is not intended to
-          override them.
+        This privacy coverage is specifically relevant as follows: (i) it governs the Platform solely and does not amplify to websites or packages of different entities, and (ii) it displays our adherence to the relevant legal guidelines and guidelines of Pakistan (the "Applicable Laws").
         </p>
-        <p className="leading-relaxed text-gray-700">
+        {/* <p className="leading-relaxed text-gray-700">
           This privacy policy: (i) applies only to the Platform and not to
           websites or applications of any other companies or organizations; and
           (ii) specifically addresses our obligations pursuant to the applicable
           laws of Pakistan (the{" "}
           <span className="font-semibold">"Applicable Laws"</span>).
-        </p>
+        </p> */}
       </div>
     );
   };
